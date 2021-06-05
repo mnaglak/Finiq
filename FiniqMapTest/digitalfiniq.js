a0e0bb1ab10460ef417124370028a7639652d4cf
tylerdjo <72691661+tylerdjo@users.noreply.github.com>
2021-06-05 17:09:05 +0200

    Update digitalfiniq.js

--- FiniqMapTest/digitalfiniq.js
+++ FiniqMapTest/digitalfiniq.js
@@ -24,6 +24,8 @@ var map = new L.map('map', mapOptions);
 
 //Example of an externally called basemap
 var Esri_WorldImagery = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
+            maxNativeZoom: 17,
+            maxZoom: 20,
             attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'});
 
 
